32432a43548495b4fed3b44bb5299b5cc0f540fb
Alexandr <a.timoff3@gmail.com>
2026-09-11 22:34:01 +0300

    finish with step 3 with install poetry and venv for them

--- checklist.py
+++ checklist.py
@@ -1,30 +1,34 @@
 # Python script which go through a checklist and prints out the items that are not completed.
 # Original checklist from TASK.md file:
-#  [ ] Python 3.9–3.13; 
+#  [ ] Python 3.9–3.12; 
 #  [ ] Poetry; 
 #  [ ] VS Code + расширения; 
 #  [ ] `poetry install` без ошибок; 
 #  [ ] проверка импортов вернула «OK».
 
 
-# Step 1. Python 3.9-3.13:
+# Step 1. Python 3.9-3.12:
 import sys
-if sys.version_info > (3, 8) and sys.version_info < (3, 14):
+if sys.version_info > (3, 8) and sys.version_info < (3, 13):
     print(f"Python version is {sys.version_info.major}.{sys.version_info.minor}")
-    print("[+] Python 3.9–3.13")
+    print("[+] Python 3.9–3.12")
 else:
-    print("Python version is not in the range 3.9-3.13")
-    raise Exception("Checklist step 1: [ ] Python 3.9–3.13 is not completed.")
+    print("Python version is not in the range 3.9-3.12")
+    raise Exception("Checklist step 1: [ ] Python 3.9–3.12 is not completed.")
 
 # Step 2. Poetry:
 # To install poetry, run the following command in your terminal:
-# curl -sSL https://install.python-poetry.org | python3 -
+#   curl -sSL https://install.python-poetry.org | python3 -
+#   poetry install
 
-try:
-    import poetry
-    print("[+] Poetry is installed.")
-except ImportError:
+
+import shutil
+print("[+] Poetry is installed.")
+poetry_path = shutil.which("poetry")
+if poetry_path is None:
     print("Poetry is not installed.")
+    print("Try to use:\t\tcurl -sSL https://install.python-poetry.org | python3 -")
+    print("Then run:\t\tpoetry install")
     raise Exception("Checklist step 2: [ ] Poetry is not completed.")
 
 # Step 3. VS Code + extensions:
